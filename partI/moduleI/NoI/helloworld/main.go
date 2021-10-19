9638fcbe070c39f252684e06b0cd3efc0bfcc075
cjx <1067446576@qq.com>
2021-10-19 22:14:15 +0800

    first commit

--- partI/moduleI/NoI/helloworld/main.go
+++ partI/moduleI/NoI/helloworld/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	// name 的默认值设为 world, (key, default_value, desc)
+	// name 的默认值设为 world,  flag.String(key, default_value, desc)
 	name := flag.String("name", "world", "specify the name you want to say hi")
 	// 解析 os.Args，将 --key 后的值赋值给 key
 	flag.Parse()
